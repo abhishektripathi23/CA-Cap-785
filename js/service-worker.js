9770dc9ed8415b24d1eda675b4ca7ef86f6e8d8b
Abhishek tripathi <150678292+abhishektripathi23@users.noreply.github.com>
2025-04-08 15:38:54 +0530

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,1 +1,50 @@
+const CACHE_NAME = 'v1-cache';
+const CACHE_URLS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+];
+
+// Install Event – Cache resources
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_URLS);
+        })
+    );
+});
+
+// Activate Event – Clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event – Serve cached content or fetch from network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request).then((response) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
 
